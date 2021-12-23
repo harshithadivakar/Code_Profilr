79453d3f05200c3aaa0c06cd1095b189ea11f5c4
harshithadivakar <54227093+harshithadivakar@users.noreply.github.com>
2021-12-23 21:06:28 +0530

    Add files via upload

---
+++ sticks.py
@@ -0,0 +1,21 @@
+num_sticks = input('Enter')
+x =list()
+if 1<= int(num_sticks)<= 10**50 :
+
+    for num in range(int(num_sticks)) : 
+        Ai = int(input('Enter Height:'))
+        if 0<=Ai<= 10 **9: 
+            x.append(Ai)
+    y = min(x)
+    for nos in x:
+        if nos > y:
+            nos  = nos -y
+        elif nos == y:
+            nos =0
+        else :
+            continue
+       
+   
+    
+
+
